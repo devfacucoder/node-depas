425aea8efcb67d010830bf71b59d17b1166307f3
facundo <facudoer@gmail.com>
2024-10-08 13:19:22 -0300

    revisando peticiones

--- src/app.js
+++ src/app.js
@@ -3,8 +3,8 @@ const app = express();
 import { createRoles } from "./libs/createRoles.js";
 import cors from "cors";
 import createAdmin from "./libs/createAdmin.js";
-import dotenv from "dotenv"
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 import userRoutes from "./routes/users.routes.js";
 import panelRoutes from "./routes/panel.routes.js";
 import depaRoutes from "./routes/depas.routes.js";
@@ -12,18 +12,20 @@ import authRoutes from "./routes/auth.routes.js";
 import imageRoutes from "./routes/image.routes.js";
 createRoles();
 const clientIP = process.env.CLIENT_IP;
-createAdmin()
-app.use(cors({
+createAdmin();
+app.use(
+  cors({
     origin: (origin, callback) => {
-        
+      console.log(clientIP);
+      console.log(origin);
       if (origin === clientIP || !origin) {
-        callback(null, true);  // Permite el acceso si la IP es correcta o si no hay "origin" (ej. Postman)
+        callback(null, true); // Permite el acceso si la IP es correcta o si no hay "origin" (ej. Postman)
       } else {
-        callback(new Error("No autorizado por CORS"));  // Bloquea si la IP no coincide
+        callback(new Error("No autorizado por CORS")); // Bloquea si la IP no coincide
       }
-    }
-  }));
-  
+    },
+  })
+);
 
 app.use(express.json());
 app.use("/api/users/", userRoutes);
