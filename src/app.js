e1abd4e7fcdc2553aad85d96353ff956f8cfff99
facundo <facudoer@gmail.com>
2024-10-08 12:51:14 -0300

    cors mejorado

--- src/app.js
+++ src/app.js
@@ -3,16 +3,27 @@ const app = express();
 import { createRoles } from "./libs/createRoles.js";
 import cors from "cors";
 import createAdmin from "./libs/createAdmin.js";
-
+import dotenv from "dotenv"
+dotenv.config()
 import userRoutes from "./routes/users.routes.js";
 import panelRoutes from "./routes/panel.routes.js";
 import depaRoutes from "./routes/depas.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import imageRoutes from "./routes/image.routes.js";
 createRoles();
-
+const clientIP = process.env.CLIENT_IP;
 createAdmin()
-app.use(cors());
+app.use(cors({
+    origin: (origin, callback) => {
+        
+      if (origin === clientIP || !origin) {
+        callback(null, true);  // Permite el acceso si la IP es correcta o si no hay "origin" (ej. Postman)
+      } else {
+        callback(new Error("No autorizado por CORS"));  // Bloquea si la IP no coincide
+      }
+    }
+  }));
+  
 
 app.use(express.json());
 app.use("/api/users/", userRoutes);
