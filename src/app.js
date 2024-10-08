c745054f9bc7d6b7a177f7e839ead1c3ba1184e0
facundo <facudoer@gmail.com>
2024-10-08 13:28:27 -0300

    lest go

--- src/app.js
+++ src/app.js
@@ -13,19 +13,25 @@ import imageRoutes from "./routes/image.routes.js";
 createRoles();
 const clientIP = process.env.CLIENT_IP;
 createAdmin();
-app.use(
-  cors({
-    origin: (origin, callback) => {
-      console.log(clientIP);
-      console.log(origin);
-      if (origin === clientIP || !origin) {
-        callback(null, true); // Permite el acceso si la IP es correcta o si no hay "origin" (ej. Postman)
-      } else {
-        callback(new Error("No autorizado por CORS")); // Bloquea si la IP no coincide
-      }
-    },
-  })
-);
+const allowedOrigins = [process.env.CLIENT_IP,"https://devfacucoder.github.io"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log('Solicitud de origen:', origin);
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      console.log("el origen es: " + origin)
+      // Permitir si el origen está en la lista de permitidos o si es una solicitud sin origen (como Postman)
+      callback(null, true);
+      
+    } else {
+      // Bloquear si el origen no está permitido
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true, // Permitir cookies o credenciales
+};
 
 app.use(express.json());
 app.use("/api/users/", userRoutes);
